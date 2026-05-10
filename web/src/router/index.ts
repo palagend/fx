1c2b7ddda962a1214165566142b3cdebdd3852eb
Arthur <palagend@foxmail.com>
2026-05-10 10:19:19 +0800

    refactor(router): 简化懒加载组件语法
    移除冗余的 defineAsyncComponent 包装，直接使用箭头函数导入组件

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,21 +1,20 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import { defineAsyncComponent } from 'vue'
 
 // 首屏关键组件同步加载
 import Home from '../components/Home.vue'
 
 // 其他组件懒加载，使用具名 chunk 合并相关模块
-const ExchangeRate = defineAsyncComponent(() => import(/* webpackChunkName: "feature-tools" */ '../components/ExchangeRate.vue'))
-const Calculator = defineAsyncComponent(() => import(/* webpackChunkName: "feature-tools" */ '../components/Calculator.vue'))
-const QRCodeGenerator = defineAsyncComponent(() => import(/* webpackChunkName: "feature-tools" */ '../components/QRCodeGenerator.vue'))
+const ExchangeRate = () => import(/* webpackChunkName: "feature-tools" */ '../components/ExchangeRate.vue')
+const Calculator = () => import(/* webpackChunkName: "feature-tools" */ '../components/Calculator.vue')
+const QRCodeGenerator = () => import(/* webpackChunkName: "feature-tools" */ '../components/QRCodeGenerator.vue')
 
-const Portfolio = defineAsyncComponent(() => import(/* webpackChunkName: "feature-portfolio" */ '../components/Portfolio.vue'))
+const Portfolio = () => import(/* webpackChunkName: "feature-portfolio" */ '../components/Portfolio.vue')
 
-const PasswordGenerator = defineAsyncComponent(() => import(/* webpackChunkName: "feature-password" */ '../components/PasswordGenerator.vue'))
-const PasswordManager = defineAsyncComponent(() => import(/* webpackChunkName: "feature-password" */ '../components/PasswordManager.vue'))
+const PasswordGenerator = () => import(/* webpackChunkName: "feature-password" */ '../components/PasswordGenerator.vue')
+const PasswordManager = () => import(/* webpackChunkName: "feature-password" */ '../components/PasswordManager.vue')
 
-const MobileTools = defineAsyncComponent(() => import(/* webpackChunkName: "feature-mobile" */ '../components/MobileTools.vue'))
-const MobileProfile = defineAsyncComponent(() => import(/* webpackChunkName: "feature-mobile" */ '../components/MobileProfile.vue'))
+const MobileTools = () => import(/* webpackChunkName: "feature-mobile" */ '../components/MobileTools.vue')
+const MobileProfile = () => import(/* webpackChunkName: "feature-mobile" */ '../components/MobileProfile.vue')
 
 const routes: RouteRecordRaw[] = [
   { 
